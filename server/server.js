93547a9ba6337146baec48cecd72b770a6062587
Vitalik <gghjbhgfbb@gmail.com>
2025-02-08 17:17:09 +0300

    second commit

--- server/server.js
+++ server/server.js
@@ -2,20 +2,20 @@ const jsonServer = require('json-server');
 const cors = require('cors');
 const path = require('path');
 
-// Создаем сервер
+
 const server = jsonServer.create();
 
-// Подключаем middleware CORS
+
 server.use(cors());
 
-// Используем стандартные middleware json-server
+
 server.use(jsonServer.defaults());
 
-// Роутер для db.json
+
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 server.use(router);
 
-// Запускаем сервер
+
 server.listen(5555, () => {
     console.log('JSON Server is running on port 5555');
 });
